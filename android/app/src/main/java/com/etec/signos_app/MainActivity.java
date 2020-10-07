2bbd3451b2f3245c76b1c3ba98bde3c71f255762
unknown <alinedsc1989@gmail.com>
2020-10-06 23:45:17 -0300

    Realizado projeto Signos_app Projeto ATV. ETEC

---
+++ android/app/src/main/java/com/etec/signos_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.etec.signos_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:signos_app/tela/signos.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Signos(),
+
+  ));
+}
